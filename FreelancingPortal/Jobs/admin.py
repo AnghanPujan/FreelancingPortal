89ee78f2ca3aa075e2a34d02f7acde594a8e7547
AnghanPujan <poojan.anghan@exhibytesolution.com>
2025-10-27 13:57:07 +0530

    Registered model in admin.py

--- FreelancingPortal/Jobs/admin.py
+++ FreelancingPortal/Jobs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Job
 
 # Register your models here.
+@admin.register(Job)
+class jobs(admin.ModelAdmin):
+    list_display = ('title', 'enterprise', "is_visible", "created_at", "updated_at")
+    search_fields = ('title', 'enterprise')
