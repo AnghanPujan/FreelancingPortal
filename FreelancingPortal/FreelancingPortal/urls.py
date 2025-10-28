d6b7dbddcbfaa144cd064786ccb4405c35f454e6
AnghanPujan <poojan.anghan@exhibytesolution.com>
2025-10-28 19:23:05 +0530

    Customized styling for index and login page

--- FreelancingPortal/FreelancingPortal/urls.py
+++ FreelancingPortal/FreelancingPortal/urls.py
@@ -15,3 +15,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Freelancing Portal Admin site"
+admin.site.site_title = "Admin Portal"
+admin.site.index_title = "Dashboard"
